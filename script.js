8cc37dec5c3d4dcb62d900be96c0824b242ac95c
Daniel <trifdaniel01@gmail.com>
2021-12-17 15:49:59 +0200

    created ui

---
+++ script.js
@@ -0,0 +1,8 @@
+const numbers = document.querySelectorAll(`[data-number]`);
+const operations = document.querySelectorAll(`[data-operation]`); 
+const ac = document.querySelector(`[data-ac]`);
+const deletebutton = document.querySelector(`[data-delete]`);
+const equal = document.querySelector(`[data-equal]`);
+const previous_operand = document.querySelector(`[data-previous-operand]`);
+const current_operand = document.querySelector(`[data-current-operand]`);
+
